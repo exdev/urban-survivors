552923cdd3cd29b59ca6e48ece6fe8d73078f243
Wu Jie (mac-pro) <Johnny@mac-book-pro.local>
2010-09-15 23:38:38 +0800

    add basic behavior tree classes
    remove unused files

--- Assets/user/jwu/script/ai/ai.js
+++ Assets/user/jwu/script/ai/ai.js
@@ -16,6 +16,9 @@ var target: Transform;
 // ------------------------------------------------------------------ 
 
 function Start () {
+    var btree : Btree = new Btree("hello foobar!");
+    // btree.init();
+    // Debug.Log( "var1 = " + btree.myvar + ", var2 = " + btree.myvar2 );
 }
 
 // ------------------------------------------------------------------ 
@@ -23,8 +26,8 @@ function Start () {
 // ------------------------------------------------------------------ 
 
 function Update () {
-    transform.position.x += Mathf.Cos(Time.time) * 10.0 * Time.deltaTime;
-    transform.position.z += Mathf.Sin(Time.time) * 10.0 * Time.deltaTime;
+    // transform.position.x += Mathf.Cos(Time.time) * 10.0 * Time.deltaTime;
+    // transform.position.z += Mathf.Sin(Time.time) * 10.0 * Time.deltaTime;
     var wanted_rot = Quaternion.LookRotation( target.position - transform.position );
     transform.rotation = Quaternion.Slerp ( transform.rotation, wanted_rot, Time.deltaTime * 4.0 );
 }
