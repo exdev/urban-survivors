6e9ee6745659c273da98f266f5d8337f836be645
Wu Jie (mac-pro) <Johnny@mac-book-pro.local>
2010-10-21 23:49:44 +0800

    add CurveTransfer script, fix Infrared collision problem.

---
+++ Assets/Editor/CurvesTransferer.cs
@@ -0,0 +1,75 @@
+// ======================================================================================
+// File         : CurvesTransferer.cs
+// Author       : Wu Jie 
+// Last Change  : 10/20/2010 | 23:04:23 PM | Wednesday,October
+// Description  : 
+// ======================================================================================
+
+///////////////////////////////////////////////////////////////////////////////
+// usings
+///////////////////////////////////////////////////////////////////////////////
+
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+///////////////////////////////////////////////////////////////////////////////
+// class CurvesTransferer 
+// 
+// Purpose: 
+// 
+///////////////////////////////////////////////////////////////////////////////
+
+public class CurvesTransferer
+{
+    const string duplicatePostfix = "_copy";
+
+    static void CopyClip(string importedPath, string copyPath)
+    {
+        AnimationClip src = AssetDatabase.LoadAssetAtPath(importedPath, typeof(AnimationClip)) as AnimationClip;
+        AnimationClip newClip = new AnimationClip();
+        newClip.name = src.name + duplicatePostfix;
+        AssetDatabase.CreateAsset(newClip, copyPath);
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Assets/Transfer Clip Curves to Copy")]
+    static void CopyCurvesToDuplicate()
+    {
+        // Get selected AnimationClip
+        AnimationClip imported = Selection.activeObject as AnimationClip;
+        if (imported == null)
+        {
+            Debug.Log("Selected object is not an AnimationClip");
+            return;
+        }
+
+        // Find path of copy
+        string importedPath = AssetDatabase.GetAssetPath(imported);
+        string copyPath = importedPath.Substring(0, importedPath.LastIndexOf("/"));
+        copyPath += "/" + imported.name + duplicatePostfix + ".anim";
+
+        CopyClip(importedPath, copyPath);
+
+        AnimationClip copy = AssetDatabase.LoadAssetAtPath(copyPath, typeof(AnimationClip)) as AnimationClip;
+        if (copy == null)
+        {
+            Debug.Log("No copy found at " + copyPath);
+            return;
+        }
+        // Copy curves from imported to copy
+        AnimationClipCurveData[] curveDatas = AnimationUtility.GetAllCurves(imported, true);
+        for (int i = 0; i < curveDatas.Length; i++)
+        {
+            AnimationUtility.SetEditorCurve(
+                                            copy,
+                                            curveDatas[i].path,
+                                            curveDatas[i].type,
+                                            curveDatas[i].propertyName,
+                                            curveDatas[i].curve
+                                           );
+        }
+
+        Debug.Log("Copying curves into " + copy.name + " is done");
+    }
+}
