ba50c845646bff4f5b6ff80a1bde6e87397e998b
Wu Jie (mac-pro) <Johnny@mac-book-pro.local>
2010-12-05 23:39:55 +0800

    use merge order for git command, prevent re-operating comitted files.

--- utility/gen_git_merge_commands.lua
+++ utility/gen_git_merge_commands.lua
@@ -1,14 +1,8 @@
 -- local file = io.open ("data.txt")
 local file = io.stdin
 
--- unmerged changes
-local added_by_them = {}
-local added_by_us = {}
-local both_modified = {}
-local both_deleted = {}
-
 if file then
-    io.write( "echo ========== process changes ==========\n" )
+    -- io.write( "echo ========== process changes ==========\n" )
 
     -- process input
     for line in file:lines() do
@@ -18,58 +12,33 @@ if file then
             -- DEBUG: io.write( merge_type .. ": " .. path .. "\n" )
 
             -- changes
-            if merge_type == "new file" then
-                io.write( "git add " .. path .. "\n" )
-            elseif merge_type == "modified" then
-                io.write( "git add " .. path .. "\n" )
-            elseif merge_type == "renamed" then
-                -- NOTE: - is a pattern character, so you have to use %-
-                local src,dest = path:match("(.+)%s%->%s(.+)")
-                io.write( "git mv " .. src .. " " .. dest .. "\n" )
-            elseif merge_type == "deleted" then
-                io.write( "git rm " .. path .. "\n" )
+            -- if merge_type == "new file" then
+            --     io.write( "git add " .. path .. "\n" )
+            -- elseif merge_type == "modified" then
+            --     io.write( "git add " .. path .. "\n" )
+            -- elseif merge_type == "renamed" then
+            --     -- NOTE: - is a pattern character, so you have to use %-
+            --     local src,dest = path:match("(.+)%s%->%s(.+)")
+            --     io.write( "git mv " .. src .. " " .. dest .. "\n" )
+            -- elseif merge_type == "deleted" then
+            --     io.write( "git rm " .. path .. "\n" )
 
             -- unmerged
-            elseif merge_type == "added by them" then
-                added_by_them[#added_by_them+1] = path
+            if merge_type == "added by them" then
+                io.write( "git checkout --theirs " .. path .. "\n" ) 
+                io.write( "git add " .. path .. "\n" ) 
             elseif merge_type == "added by us" then 
-                added_by_us[#added_by_us+1] = path
+                io.write( "git checkout --ours " .. path .. "\n" ) 
+                io.write( "git add " .. path .. "\n" ) 
             elseif merge_type == "both deleted" then 
-                both_deleted[#both_deleted+1] = path
+                io.write( "git rm " .. path .. "\n" ) 
             elseif merge_type == "both modified" then 
-                both_modified[#both_modified+1] = path
+                io.write( "git checkout --theirs " .. path .. "\n" ) 
+                io.write( "git add " .. path .. "\n" ) 
             end
         end
     end
-
-    -- process output
-    io.write( "echo ========== process merges ==========\n" )
-    io.write( "echo ---------- added by them ----------\n" )
-    for i=1,#added_by_them do 
-        local path = added_by_them[i]
-        io.write( "git checkout --theirs " .. path .. "\n" ) 
-        io.write( "git add " .. path .. "\n" ) 
-    end
-
-    io.write( "echo ---------- added by us ----------\n" )
-    for i=1,#added_by_us do 
-        local path = added_by_us[i]
-        io.write( "git checkout --ours " .. path .. "\n" ) 
-        io.write( "git add " .. path .. "\n" ) 
-    end
-
-    io.write( "echo ---------- both modified ----------\n" )
-    for i=1,#both_modified do 
-        local path = both_modified[i]
-        io.write( "git checkout --theirs " .. path .. "\n" ) 
-        io.write( "git add " .. path .. "\n" ) 
-    end
-
-    io.write( "echo ---------- both deleted ----------\n" ) -- NOTE: always manipluate delete at last
-    for i=1,#both_deleted do 
-        local path = both_deleted[i]
-        io.write( "git rm " .. path .. "\n" ) 
-    end
+    io.write( "git add .\n" ) 
 else
     assert ( false, "failed to open file data.txt" )
 end
